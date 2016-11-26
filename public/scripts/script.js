c8269a4336e8c8f8097fe77cd33b010ba336bffa
HenryHall <mnboogie@yahoo.com>
2016-11-26 17:10:39 -0600

    camera unlock and small changes

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,11 +1,35 @@
 var myApp = angular.module('myApp', []);
 
+var currentZoom = 1;
 
 myApp.controller('createCardController', ['$scope', '$http', function($scope, $http){
 
   $scope.allSets = {};
   $scope.createdCardArray = [];
   $scope.uniqueID = 0;
+  $scope.ui = {};
+  $scope.ui.hidden = false;
+  $scope.zoom = currentZoom * 100;
+  $scope.gameBoardStyle = {
+    transform: 'scale(' + currentZoom + ')'
+  };
+
+  $scope.gameStats = {
+    opponent: {
+      life: 20,
+      hand: [],
+      graveyard: [],
+      exile: [],
+      library: []
+    },
+    self: {
+      life: 20,
+      hand: [],
+      graveyard: [],
+      exile: [],
+      library: []
+    }
+  };
 
   $http({
     method: 'GET',
@@ -22,7 +46,15 @@ myApp.controller('createCardController', ['$scope', '$http', function($scope, $h
   };
 
 
+  $scope.toggleUI = function(){
+    console.log("toggle");
+    $scope.ui.hidden = !$scope.ui.hidden;
+  };
+
+
   $scope.createNewCard = function(key){
+
+    console.log($scope.newCardModel);
 
     //Make sure there is text to create a card
     if($scope.newCardModel == "" || $scope.newCardModel == undefined){
@@ -75,13 +107,14 @@ myApp.controller('createCardController', ['$scope', '$http', function($scope, $h
     if(newCard.info.layout == "double-faced"){
       newCard.night = {};
       newCard.night.info = getCardInfo(newCard.info.names[1]);
+      newCard.night.uniqueID = $scope.uniqueID;
       newCard.night.style = {
         "background-Image": "url('http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + cardInfo.multiverseid + "&type=card')",
         "background-size": "cover",
         "background-color": "trasnparent",
         height: '155px',
         width: '111px',
-        'z-index': 100
+        'z-index': 100,
       };
       if(newCard.night.info.types.indexOf("Creature") != -1) {
         newCard.night.powerToughness = newCard.night.info.power + "/" + newCard.night.info.toughness;
@@ -135,9 +168,27 @@ myApp.controller('createCardController', ['$scope', '$http', function($scope, $h
       $scope.createdCardArray = [];
       $scope.uniqueID = 0;
     }
-
-    return;
   };
+
+
+  $scope.zoomIn = function(){
+    if (currentZoom >= 2) {
+      return;
+    }
+    currentZoom = currentZoom + .1;
+    $scope.gameBoardStyle.transform = 'scale(' + currentZoom + ')';
+    $scope.zoom = Math.floor(currentZoom * 100);
+  }
+
+
+  $scope.zoomOut = function(){
+    if (currentZoom <= .6) {
+      return;
+    }
+    currentZoom = currentZoom - .1;
+    $scope.gameBoardStyle.transform = 'scale(' + currentZoom + ')';
+    $scope.zoom = Math.floor(currentZoom * 100);
+  }
 
 
   $scope.tapCard = function(card){
@@ -181,18 +232,17 @@ myApp.controller('createCardController', ['$scope', '$http', function($scope, $h
   };
 
 
-  $scope.flipDFCard = function(index){
-    //rotate the card
-    // var currentCard = angular.element($event.target).parent().parent().parent();
-    // currentCard.addClass('rotating');
+  $scope.flipDFCard = function(index, event){
+    var thisElementBounds = event.target.getBoundingClientRect();
 
     var tempCard = $scope.createdCardArray.splice(index, 1);
     var length = $scope.createdCardArray.push(tempCard[0].night);
     $scope.createdCardArray[length-1].night = tempCard[0];
+    $scope.createdCardArray[length-1].style.transform = 'scale(' + currentZoom + ')';
+    $scope.createdCardArray[length-1].style.top = thisElementBounds.top;
+    $scope.createdCardArray[length-1].style.left = thisElementBounds.left;
     $scope.createdCardArray[length-1].style['background-Image'] = "url('http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + tempCard[0].night.info.multiverseid  + "&type=card')";
     delete $scope.createdCardArray[length-1].night['night'];
-    console.log(tempCard);
-    console.log($scope.createdCardArray[index]);
 
   };
 
@@ -223,30 +273,46 @@ myApp.controller('createCardController', ['$scope', '$http', function($scope, $h
   };
 
 
+  $scope.incrementSelf = function(){
+    $scope.gameStats.self.life++;
+  };
+
+
+  $scope.decrementSelf = function(){
+    $scope.gameStats.self.life--;
+  };
+
+
+  $scope.incrementOpponent = function(){
+    $scope.gameStats.opponent.life++;
+  };
+
+
+  $scope.decrementOpponent = function(){
+    $scope.gameStats.opponent.life--;
+  };
+
+
 }])
-// .directive('rotating', function(){
-//   restrict: 'C',
-//   scope: {rotation: 0},
-//   link: function(scope, element, attrs){
-//     element.css({
-//       transform: "rotate(" + $scope.rotation + "deg)"
-//     });
-//     if(element){
-//       function(){
-//         for(var i=0; i<91; i++){
-//           $scope.rotation = i;
-//         }
-//         for(var i=90; i>-1; i++){
-//           $scope.rotation = i;
-//         }
-//       }
-//     }
-//   }
-// })
-.directive('uiPanel', function(){
+.directive('cardPortal', function(){
   return {
     restrict: 'E',
-    templateUrl: '../views/templates/uiPanel.html'
+    templateUrl: '../views/templates/cardPortal.html',
+    controllerAs: 'createCardController',
+    replace: true
+  }
+})
+.directive('gameBoardUi', function(){
+  return {
+    restrict: 'E',
+    templateUrl: '../views/templates/gameBoardUi.html',
+    controllerAs: 'createCardController'
+  }
+})
+.directive('gameStats', function(){
+  return {
+    restrict: 'E',
+    templateUrl: '../views/templates/gameStats.html'
   }
 })
 .directive('customContextMenu', function(){
@@ -264,14 +330,24 @@ myApp.controller('createCardController', ['$scope', '$http', function($scope, $h
         element.css({
           transform: "rotate(-90deg)",
           top: '-20px',
-          left: '75px',
-          // 'z-index': 1000
+          left: '75px'
         });
       }
     }
   }
 })
-.directive('ngRightClick', function($parse, $compile) {
+.directive('ngRightClick', function($parse) {
+    return function(scope, element, attrs) {
+        var fn = $parse(attrs.ngRightClick);
+        element.bind('contextmenu', function(event) {
+            scope.$apply(function() {
+                event.preventDefault();
+                fn(scope, {$event:event});
+            });
+        });
+    };
+})
+.directive('contextmenurightclick', function($parse, $compile) {
     return function(scope, element, attrs) {
         // var fn = $parse(attrs.ngRightClick);
         element.bind('contextmenu', function(event) {
@@ -287,21 +363,117 @@ myApp.controller('createCardController', ['$scope', '$http', function($scope, $h
         });
     };
 })
+.directive('carddraggable', function($document) {
+  //From AngularJS directive example page
+  return {
+    restrict: 'A',
+    scope: {},
+    link: function(scope, element, attr) {
+        var startX = 0, startY = 0;
+
+        //calculate where the card portal is
+        var cardPortal = document.getElementById('cardPortal').getBoundingClientRect();
+        var gameBoard = document.getElementById('gameBoard').getBoundingClientRect();
+        console.log("gameboard", gameBoard);
+        console.log("cardPortal", cardPortal);
+        var y = (gameBoard.bottom - gameBoard.top)/4 + (cardPortal.top/currentZoom) + (125/currentZoom);
+        var x = (gameBoard.right - gameBoard.left)/4 + (cardPortal.left/currentZoom) + (45/currentZoom);
+        element.css({
+          top: y + 'px',
+          left: x + 'px'
+        });
+
+        element.css({
+         position: 'absolute',
+         cursor: 'pointer',
+         display: 'block'
+        });
+
+        element.on('mousedown', function(event) {
+          // Prevent default dragging of selected content
+          event.preventDefault();
+
+          startX = (event.clientX/currentZoom) - x;
+          startY = (event.clientY/currentZoom) - y;
+
+          // console.log('x', x);
+          // console.log('y', y);
+          // console.log('startX', startX);
+          // console.log('startY', startY);
+          // console.log('event x', event.clientX);
+          // console.log('event y', event.clientY);
+          $document.on('mousemove', mousemove);
+          $document.on('mouseup', mouseup);
+        });
+
+
+        function mousemove(event) {
+          // console.log('x', x);
+          // console.log('y', y);
+          // console.log('startX', startX);
+          // console.log('startY', startY);
+          // console.log('event x', event.clientX);
+          // console.log('event y', event.clientY);
+
+
+            y = ((event.clientY/currentZoom) - startY);
+            x = ((event.clientX/currentZoom) - startX);
+
+            // console.log('x', x);
+            // console.log('y', y);
+            // console.log('startX', startX);
+            // console.log('startY', startY);
+            // console.log('event x', event.clientX);
+            // console.log('event y', event.clientY);
+            element.css({
+              top: y + 'px',
+              left:  x + 'px'
+            });
+        }
+
+
+        function mouseup() {
+          $document.off('mousemove', mousemove);
+          $document.off('mouseup', mouseup);
+        }
+      }
+    }
+})
 .directive('draggable', function($document) {
   //From AngularJS directive example page
   return function(scope, element, attr) {
-    var startX = 0, startY = 0, x = 0, y = 0;
+    var startX = 150, startY = 0, x = 150, y = 0;
+
+    //Set the starting coords for the gameBoard
+    if(element[0].id == "gameBoard"){
+      var gameBoard = document.getElementById('gameBoard').getBoundingClientRect();
+      console.log(gameBoard);
+      startX = gameBoard.left;
+      startY = gameBoard.top;
+      x = gameBoard.left;
+      y = gameBoard.top;
+    }
+
     element.css({
-     position: 'relative',
+     position: 'absolute',
      cursor: 'pointer',
-     display: 'block'
+     display: 'block',
+     left: x + 'px'
     });
     element.on('mousedown', function(event) {
       // Prevent default dragging of selected content
-      console.log(event);
-      event.preventDefault();
-      startX = event.screenX - x;
-      startY = event.screenY - y;
+      if(event.target.tagName != "INPUT"){
+        event.preventDefault();
+      }
+
+      //make sure the gameBoard doesnt move when a card is dragged
+      if(event.target.classList.contains('createdCard')){
+        return;
+      }
+
+      startX = (event.clientX - x);
+      startY = (event.clientY - y);
+
       $document.on('mousemove', mousemove);
       $document.on('mouseup', mouseup);
     });
@@ -309,53 +481,13 @@ myApp.controller('createCardController', ['$scope', '$http', function($scope, $h
 
     function mousemove(event) {
 
-      //Make sure the element can't leave the gameBoard element
-      var elementBounds = event.target.getBoundingClientRect();
-      var gameBoard = document.getElementById('gameBoard').getBoundingClientRect();
+        y = (event.clientY - startY);
+        x = (event.clientX - startX);
 
-      console.log("elementBounds", elementBounds);
-      console.log("gameBoard", gameBoard);
-      console.log("element", element);
-
-      //correction for a weird glitch
-      if(elementBounds.left == 0){
-        console.log("out of bounds");
-        return
-      }
-
-
-      if(elementBounds.left < gameBoard.left + 10){
-        element.css({
-          left: (gameBoard.left + 15) + 'px'
-        });
-        x = gameBoard.left + 15;
-        $document.off('mousemove', mousemove);
-      } else if (elementBounds.right > gameBoard.right - 10){
-        element.css({
-          left: (elementBounds.right - 160) + 'px'
-        });
-        x = elementBounds.right - 160;
-        $document.off('mousemove', mousemove);
-      } else if (elementBounds.top < gameBoard.top + 10){
-        element.css({
-          top: 15 + 'px'
-        });
-        y = 15;
-        $document.off('mousemove', mousemove);
-      } else if (elementBounds.bottom > gameBoard.bottom - 10){
-        element.css({
-          top: (elementBounds.top - 200) + 'px'
-        });
-        y = elementBounds.top - 200;
-        $document.off('mousemove', mousemove);
-      } else {
-        y = event.screenY - startY;
-        x = event.screenX - startX;
         element.css({
           top: y + 'px',
           left:  x + 'px'
         });
-      }
     }
 
 
