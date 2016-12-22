29e64fcd6be9207579917bc9b40f5ff2852af303
HenryHall <mnboogie@yahoo.com>
2016-12-22 11:47:22 -0600

    home sidebar update

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -21,44 +21,50 @@ myApp.controller('homeController', ['$scope', '$http', function($scope, $http){
       data.newest[i].info.hash = data.newest[i].hashed_id;
       $scope.recentBS.push(data.newest[i].info);
     }
+
+    for (var i=0; i<data.popular.length; i++){
+      data.popular[i].info.hash = data.popular.hashed_id;
+      $scope.popularBS.push(data.popular[i].info);
+    }
+
   });
 
 
   //DO THIS
   $scope.tutorialLink = "NotMadeYet";
 
-  $scope.popularBS = [
-    {
-      title: 'Tarmogoyf and Lightning Bolt',
-      category: 'Rules Question',
-      username: 'Day[J]',
-      date: '12/14/16'
-    },
-    {
-      title: 'How can I stop this',
-      category: 'Rules Question',
-      username: 'Comatose',
-      date: '12/12/16'
-    },
-    {
-      title: 'I can\'t believe this',
-      category: 'Story',
-      username: 'Kingler',
-      date: '12/13/16'
-    },
-    {
-      title: 'What happens here',
-      category: 'Rules Question',
-      username: 'Ziggy Stardew',
-      date: '12/14/16'
-    },
-    {
-      title: 'Sick decision making',
-      category: 'Story',
-      username: 'Robo Dick',
-      date: '12/8/16'
-    }
-  ];
+  // $scope.popularBS = [
+  //   {
+  //     title: 'Tarmogoyf and Lightning Bolt',
+  //     category: 'Rules Question',
+  //     username: 'Day[J]',
+  //     date: '12/14/16'
+  //   },
+  //   {
+  //     title: 'How can I stop this',
+  //     category: 'Rules Question',
+  //     username: 'Comatose',
+  //     date: '12/12/16'
+  //   },
+  //   {
+  //     title: 'I can\'t believe this',
+  //     category: 'Story',
+  //     username: 'Kingler',
+  //     date: '12/13/16'
+  //   },
+  //   {
+  //     title: 'What happens here',
+  //     category: 'Rules Question',
+  //     username: 'Ziggy Stardew',
+  //     date: '12/14/16'
+  //   },
+  //   {
+  //     title: 'Sick decision making',
+  //     category: 'Story',
+  //     username: 'Robo Dick',
+  //     date: '12/8/16'
+  //   }
+  // ];
 
 
   // $scope.recentBS = [
